5ead5d2165a9851b169774d77c62111854851d2e
Michael Danko <MichaelDanko@icloud.com>
2016-07-10 14:40:37 -0400

    Added router to the app.js and created div for the dyanmic view in index.html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 /* jslint browser: true */
+
+var angular = require('angular');
+var angularRoute = require('angular-route');
+
+
+var app = angular.module('boltApp', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/welcome', {
+            controller: 'Welcome',
+            templateUrl: 'sections/welcome.html'
+        })
+        .when('/new-user', {
+            controller: 'NewUser',
+            template: 'sections/newuser.html'
+        })
+        .when('/today/:account', {
+            controller: 'TodayView',
+            templateUrl: 'sections/today.html'
+        })
+        .when('/history', {
+            templateUrl: 'sections/history.html'
+        });
+}]);
