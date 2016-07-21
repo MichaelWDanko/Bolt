6d21b55514cc952a2924115e314d9c19cf7fecc7
Michael Danko <MichaelDanko@icloud.com>
2016-07-21 16:25:36 -0400

    Added ProgressBar.JS and set up rough skeleton of the functioning ProgressBar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /* jslint browser: true */
 var angular = require('angular');
 var angularRoute = require('angular-route');
-
+var ProgressBar = require('progressbar.js');
 
 var app = angular.module('boltApp', ['ngRoute', 'BoltService']);
 
@@ -32,33 +32,66 @@ app.config(['$routeProvider', function ($routeProvider) {
 
 
 app.controller('WelcomeController', ['$scope', '$http', 'BoltService', '$routeParams', function ($scope, $http, BoltService, $routeParams) {
-    
+
+
     console.log('Switched to WelcomeController');
-    
+    var login = document.getElementById('loginbutton');
+    var create = document.getElementById('createaccountbutton');
+
+    login.addEventListener('click', function () {
+        console.log('Login button clicked');
+    });
+
+    create.addEventListener('click', function () {
+        console.log('Create button clicked');
+    });
+
+
     $scope.test = 'welcome';
-    
+
 }]);
 
 app.controller('NewUserController', ['$scope', '$http', 'BoltService', '$routeParams', function ($scope, $http, BoltService, $routeParams) {
-    
+
     console.log('Switched to NewUserController');
-    
+
     $scope.test = 'new';
-    
+
 }]);
 
 app.controller('TodayController', ['$scope', '$http', 'BoltService', '$routeParams', function ($scope, $http, BoltService, $routeParams) {
-    
+
     console.log('Switched to TodayController');
+
+    var semiCircle = new ProgressBar.SemiCircle('#container', {
+        color: '#FCB03C',
+        strokeWidth: 3,
+        trailWidth: 1,
+//        text: {
+//            value: '0'
+//        }
+    });
+    
+    var animate = document.getElementById('animate-btn');
+    animate.addEventListener('click', function () {
+        console.log('clicked animated');
+        semiCircle.animate('.8', {
+            duration: 1000,
+        });
+        semiCircle.setText(80);
+    });
+
+    
+    
     
     $scope.test = 'today';
-    
+
 }]);
 
 app.controller('HistoryController', ['$scope', '$http', 'BoltService', '$routeParams', function ($scope, $http, BoltService, $routeParams) {
-    
+
     console.log('Switched to HistoryController');
-    
+
     $scope.test = 'history';
-    
+
 }]);
